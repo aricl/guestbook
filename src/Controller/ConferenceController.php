e2b08ba53a80aaa72bc5725734d9690da4cbef01
Adam <worldelephantseal@gmail.com>
2020-06-27 18:46:10 +0100

    Created a CommentRepository and a getCommentPaginator() method to this class. This is used in the ConferenceController::show() method to display comments for a conference in a paginated format

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -46,11 +49,16 @@ class ConferenceController // By not extending AbstractController you can custom
      * @Route("/conference/{id}", name="conference", methods={"GET"})
      * @return Response
      */
-    public function show(Conference $conference)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
         return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments' => $conference->getComments(),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -10,9 +10,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * ConferenceRepository
  *
- * @author    Gigabyte Software Limited
- * @copyright Gigabyte Software Limited
- *
  * @method Conference|null find($id, $lockMode = null, $lockVersion = null)
  * @method Conference|null findOneBy(array $criteria, array $orderBy = null)
  * @method Conference[]    findAll()
