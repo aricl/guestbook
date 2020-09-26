0c5b4ef0776520ed6046729c2ccedc3e4ce7ef98
Adam <worldelephantseal@gmail.com>
2020-09-26 22:47:15 +0100

    Added the ConferenceControllerTest::testCommentSubmission() test method

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -27,4 +27,21 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019 Conference');
     }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', 'conference/amsterdam-2019');
+        $client->submitForm('Submit',
+            [
+                'form[author]' => 'Matt Cousins',
+                'form[text]' => 'Upside-down with Matt',
+                'form[emailAddress]' => 'ring@lock.com',
+                'form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+            ]
+        );
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
 }
