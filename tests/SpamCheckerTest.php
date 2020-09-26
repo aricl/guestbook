c43dee01423f47b497e08a67ec0403dfc527c685
Adam <worldelephantseal@gmail.com>
2020-09-26 14:52:55 +0100

    Added the SpamCheckerTest::testGetSpamScore_withInvalidResponseReturned() test method

--- tests/SpamCheckerTest.php
+++ tests/SpamCheckerTest.php
@@ -7,6 +7,7 @@ use App\Entity\Comment;
 use App\Entity\Conference;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
 
 class SpamCheckerTest extends TestCase
 {
@@ -24,5 +25,28 @@ class SpamCheckerTest extends TestCase
         $spamScore = $spamChecker->getSpamScore($comment, []);
 
         $this->assertEquals(0, $spamScore);
+    }
+
+    public function testGetSpamScore_withInvalidResponseReturned()
+    {
+        $mockResponse = new MockResponse(
+            'invalid response body',
+            ['response_headers' => ['x-akismet-debug-help: Invalid key']]
+        );
+        $mockHttpClient = new MockHttpClient([$mockResponse]);
+        $spamChecker = new SpamChecker($mockHttpClient, 'not a real akismet key. No, not by a long shot');
+
+        $conference = new Conference('Helsinki', true, 2020);
+        $comment = Comment::createWithoutPhoto(
+            $conference,
+            'Santeri',
+            'Some comment or other',
+            'fake.email@example.com'
+        );
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to check for spam: invalid response body (Invalid key)');
+
+        $spamChecker->getSpamScore($comment, []);
     }
 }
