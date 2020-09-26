0adba067e351d8da8b6e9824cc93f1aaaf432c10
Adam <worldelephantseal@gmail.com>
2020-09-26 17:36:22 +0100

    Created development fixtures in the AppFixtures class. This was carried out using the doctrine/doctrine-fixtures-bundle

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+    private EncoderFactoryInterface $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $admin = new Admin();
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setUsername('admin');
+        $admin->setPassword($this->encoderFactory->getEncoder(Admin::class)->encodePassword('admin', null));
+        $manager->persist($admin);
+
+        $amsterdam = new Conference(
+            'Amsterdam',
+            2019,
+            true
+        );
+        $manager->persist($amsterdam);
+
+        $paris = new Conference(
+            'Paris',
+            2020,
+            true
+        );
+        $manager->persist($paris);
+
+        $comment = Comment::createWithoutPhoto(
+            $amsterdam,
+            'Fabien',
+            'Hot damn! That\'s one hell of a conference!',
+            'fabien@example.com'
+        );
+        $manager->persist($comment);
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
