88c7fa0b6870c657c6d05dcfee7c14ed7ae05956
Adam <worldelephantseal@gmail.com>
2020-09-26 14:45:15 +0100

    Added a basic SpamCheckerTest::testGetSpamScore() test method

--- tests/SpamCheckerTest.php
+++ tests/SpamCheckerTest.php
@@ -2,12 +2,27 @@
 
 namespace App\Tests;
 
+use App\Api\SpamChecker;
+use App\Entity\Comment;
+use App\Entity\Conference;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
 
 class SpamCheckerTest extends TestCase
 {
-    public function testSomething()
+    public function testGetSpamScore()
     {
-        $this->assertTrue(true);
+        $spamChecker = new SpamChecker(new MockHttpClient(), 'not a real akismet key. No, not by a long shot');
+
+        $conference = new Conference('Helsinki', true, 2020);
+        $comment = Comment::createWithoutPhoto(
+            $conference,
+            'Santeri',
+            'Some comment or other',
+            'fake.email@example.com'
+        );
+        $spamScore = $spamChecker->getSpamScore($comment, []);
+
+        $this->assertEquals(0, $spamScore);
     }
 }
