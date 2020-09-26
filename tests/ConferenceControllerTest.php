46d833df921d103113209a1b5b8af3fe2faf7b6c
Adam <worldelephantseal@gmail.com>
2020-09-26 18:01:08 +0100

    Added a ConferenceControllerTest::testConferencePage() test method. This required passing the AKISMET_KEY environment variable into the phpunit.xml.dist

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -14,4 +14,17 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019 Conference');
+    }
 }
